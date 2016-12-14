80b3dfd008f0d76d21ba36457c706336cc1f7949
thomas <dui1cuo@126.com>
2016-12-14 16:24:05 +0800

    [Mod]minor fix

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WeChatCameraDemo
 仿微信拍小视频Demo(cocoachina资源)
+
+
+
+#####效果图如下
+
+![效果如下](https://github.com/zfx5130/WeChatCameraDemo/blob/master/test.gif)
---
+++ WeChatSightDemo/WeChatSightDemo/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  WeChatSightDemo
+//
+//  Created by 吴珂 on 16/8/19.
+//  Copyright © 2016年 吴珂. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
